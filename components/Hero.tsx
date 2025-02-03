49bef70f0932c5b825fab314032eb22eef6b72c0
ibrahim <ibrahimhaidersonics777@gmail.com>
2025-02-03 10:27:20 +0300

    first steps Next.js themes lib / aceternity initialising

---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-left-10 -top-40 h-screen md:-left-32 md:-top-20"
+          fill="white"
+        />
+        <Spotlight
+          className="left-full top-10 h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="relative flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black-100 dark:bg-grid-white/[0.3]">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
